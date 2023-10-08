50a341cb422df3eeca7b5eea9219e22a43f724bd
Jasroj <basurarojas224@gmail.com>
2023-10-08 07:43:42 -0600

    Class Entity

---
+++ Proyecto1IIIC/src/Model/Entity.java
@@ -0,0 +1,131 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author JRS
+ */
+public class Entity {
+
+    private int id;
+    private int legalId;
+    private String name;
+    private String email;
+    private int phoneNumber;
+    private String address;
+    private String description;
+
+    public Entity(int id, int legalId, String name, String email, int phoneNumber, String address, String description) {
+        this.id = id;
+        this.legalId = legalId;
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.description = description;
+
+    }
+    public Entity(){}
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the legalId
+     */
+    public int getLegalId() {
+        return legalId;
+    }
+
+    /**
+     * @param legalId the legalId to set
+     */
+    public void setLegalId(int legalId) {
+        this.legalId = legalId;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the phoneNumber
+     */
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    /**
+     * @param phoneNumber the phoneNumber to set
+     */
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
